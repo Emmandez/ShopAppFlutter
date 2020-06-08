bfca5036c504dfd028c26d3d9878742b8c564214
Emmanuel Hernandez <eherdezmoran@gmail.com>
2020-06-08 14:14:32 -0500

    Animations added

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/auth.dart';
+import '../helpers/custom_route.dart';
 
 import '../screens/orders_screen.dart';
 import '../screens/user_products_screen.dart';
@@ -46,7 +47,10 @@ class MainDrawer extends StatelessWidget {
           SizedBox(height: 20),
           buildListTile(context, 'Shop', Icons.shop, ()=>Navigator.of(context).pushReplacementNamed('/')),
           Divider(thickness: 1,),
-          buildListTile(context, 'Orders', Icons.payment, ()=>Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName)),
+          buildListTile(context, 'Orders', Icons.payment, (){
+            //Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName)),
+            Navigator.of(context).pushReplacement(CustomRoute(builder: (ctx) => OrdersScreen(),));
+          }),
           Divider(thickness: 1,),
           buildListTile(context, 'Manage Products', Icons.edit, ()=>Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName)),
           Divider(thickness: 1,),
